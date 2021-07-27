7a021c11b9746d87736a6b802e64abda4043ca2a
Samuel Bennett <samuel.paul.bennett@gmail.com>
2021-07-27 17:01:36 -0600

    Separate DEV & PROD project loads
    DEV: No volumes to recreate DB everytime
    DEV: script dirs are mounted for code changes
    PROD: DB & logs persist in volumes
    
    The shell commands are improved to run Modes.
    Now you can run js file directly for data loads.

--- mongodb/setup.sh
+++ mongodb/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mongo=( mongo --port 27017 )
+mongo=( mongosh --port 27017 )
 mongo+=(
     -u $MONGO_INITDB_ROOT_USERNAME
     -p $MONGO_INITDB_ROOT_PASSWORD
@@ -9,13 +9,13 @@ mongo+=(
 CREATE_FILES=/scripts/*-create.js 
 for f in $CREATE_FILES; do
     echo "Running $f...";
-    cat $f | "${mongo[@]}";
+    "${mongo[@]}" < $f;
     echo "Done with $f...";
 done
 
 INSERT_FILES=/scripts/*-insert.js 
 for f in $INSERT_FILES; do
     echo "Running $f...";
-    "${mongo[@]}" $f;
+    "${mongo[@]}" < $f;
     echo "Done with $f...";
 done
