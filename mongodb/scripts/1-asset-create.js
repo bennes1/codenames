037ea095af20ebd27058ac044499d5bfe39b7121
Samuel Bennett <samuel.paul.bennett@gmail.com>
2021-08-01 18:46:25 -0600

    Add asset schema
    This schema is for codename assets.  The data can be either a word
    or a picture.  Later schemas for defining games will have
    references to these assets.  Random picks will be done one time &
    stored in the game document on game creation.

---
+++ mongodb/scripts/1-asset-create.js
@@ -0,0 +1,32 @@
+
+use codenames;
+
+//Set up the schema
+db.createCollection("asset", {
+	validator: {
+		$jsonSchema: {
+			bsonType: "object",
+			required: ["type", "name", "value", "collection"],
+			properties: {
+				type: {
+					enum: ["word", "picture"],
+					description: "This must be word or picture."
+				},
+				name: {
+					bsonType: "string",
+					description: "The word or the picture source filepath."
+				},
+				value: {
+					bsonType: "string",
+					description: "This contains the word or picture used."
+				},
+				collection: {
+					bsonType: "string",
+					description: "The collection that the asset was uploaded as."
+				}
+			}
+		}
+	}
+});
+
+db.asset.createIndex([{"type" : 1}, {"name" : 1}], {unique : true});
